4e71403a0e39a7e3eca9219c2c97d2276a774f08
thislife_hyeon <vkhkhv@naver.com>
2024-03-22 20:42:19 -0700

    20240323_location update & ipgo, expchange update

--- controller/delete_Location.js
+++ controller/delete_Location.js
@@ -24,19 +24,37 @@ const delete_location = async (req, res) => {
   const { code } = req.params; // URL에서 code 파라미터 추출
 
   // SQL 쿼리: 지정된 id의 로케이션 삭제
-  const sql = 'DELETE FROM location WHERE code = ?';
+  const selectProductSql = `SELECT * FROM product WHERE location = ?;`;
 
-  product_db.query(sql, [code], (err, result) => {
+  product_db.query(selectProductSql, [code], (err, result) => {
     if (err) {
       console.error('MySQL 쿼리 오류:', err);
       return res.status(500).send('서버 오류');
     }
-    if (result.affectedRows === 0) {
-      return res.status(404).send('해당 code를 가진 로케이션을 찾을 수 없습니다.');
+    if (result.length > 0) {
+      return res.status(409).send('해당 로케이션에 진열된 상품을 제거하세요.');
+    } else {
+      const sql = 'DELETE FROM location WHERE code = ?';
+
+      product_db.query(sql, [code], (err, result) => {
+        if (err) {
+          console.error('MySQL 쿼리 오류:', err);
+          return res.status(500).send('서버 오류');
+        }
+        if (result.affectedRows === 0) { //affectedRows : 데이터베이스 작업의 결과로 반환되는 속성 ,쿼리가 실행되었을 때 영향을 받은 레코드(행)의 수
+          return res.status(404).send('해당 code를 가진 로케이션을 찾을 수 없습니다.');
+        }
+        res.send('로케이션이 성공적으로 삭제되었습니다.');
+      });
     }
-    res.send('로케이션이 성공적으로 삭제되었습니다.');
   });
 
+
+
+
 };
+
+
+
 
 module.exports = { delete_location };
--- controller/ipgoHandler.js
+++ controller/ipgoHandler.js
@@ -62,21 +62,21 @@ app.post('/api/ipgo', (req, res) => {
 
 const handleIpgoRequest = async (req, res) => {
   
-    const receivedDataArray = req.body;
-    console.log('Received data from client:', receivedDataArray);
+    const receivedData = req.body;
+    console.log('Received data from client:', receivedData);
 
     // 모든 데이터를 한 번에 처리하기 위해 빈 배열을 선언 << 자꾸 송신을 여러번 보내려고 해서 추가함
     const insertionResults = [];
 
     try {
-      for (const obj of receivedDataArray) {
-        const { productcode, quantity, mfg, exp } = obj;
+      //for (const obj of receivedDataArray) {
+        //const { productcode, quantity, mfg, exp } = obj;
 
 
         // 받은 상품코드 값으로 어떤 상품인지 확인하는 데이터베이스 쿼리
         const searchResult = await new Promise((resolve, reject) => {
           const searchquery = `SELECT * FROM product_master WHERE product_code = ?`;
-          product_db.query(searchquery, [productcode], (err, result) => {
+          product_db.query(searchquery, [receivedData.productcode], (err, result) => {
             if (err) {
               console.error('MySQL 쿼리 오류:', err);
               reject(err);
@@ -87,10 +87,10 @@ const handleIpgoRequest = async (req, res) => {
           });
         });
 
-              // mfg와 exp가 null인 경우 임의의 날짜 데이터로 대체(오늘)
+              // mfg와 exp가 null인 경우 임의의 날짜 데이터로 대체(오늘) - 일단 사용 안함
       const currentDate = new Date().toISOString().split('T')[0];
-      const formattedMfg = mfg || currentDate;
-      const formattedExp = exp || currentDate;
+      const formattedMfg = receivedData.mfg || currentDate;
+      const formattedExp = receivedData.exp || currentDate;
 
       // 입력된 PK 값으로 이미 존재하는 레코드를 검색
       const ProductSearchResult = await new Promise((resolve, reject) => {
@@ -98,9 +98,9 @@ const handleIpgoRequest = async (req, res) => {
         `SELECT *
         FROM product
         WHERE product_code = ? AND mfg = ? AND exp = ? AND location = ?`;
-        product_db.query(productsearchquery, [productcode, formattedMfg, formattedExp, 'RCV'], (err, result) => {
+        product_db.query(productsearchquery, [receivedData.productcode, receivedData.mfg, receivedData.exp, 'RCV'], (err, result) => {
           if (err) {
-            console.error('MySQL 쿼리 오류:', err);
+            console.error('ProductSearchResult 쿼리 오류:', err);
             reject(err);
           } else {
             console.log('PK로 조회 성공:', result[0]);
@@ -112,15 +112,15 @@ const handleIpgoRequest = async (req, res) => {
 
       if (ProductSearchResult) {
         // 검색 결과가 있으면 해당 제품의 수량을 업데이트
-        const updatedQuantity = ProductSearchResult.quantity + Number(quantity); // 기존 수량에 추가할 수량을 더함
+        const updatedQuantity = ProductSearchResult.quantity + Number(receivedData.quantity); // 기존 수량에 추가할 수량을 더함
         const updateQuery = `
           UPDATE product
           SET quantity = ?
           WHERE product_code = ? AND mfg = ? AND exp = ?
         `;
-        product_db.query(updateQuery, [updatedQuantity, productcode, formattedMfg, formattedExp], (err, result) => {
+        product_db.query(updateQuery, [updatedQuantity, receivedData.productcode, receivedData.mfg, receivedData.exp], (err, result) => {
           if (err) {
-            console.error('MySQL 쿼리 오류:', err);
+            console.error('updateQuery 쿼리 오류:', err);
           } else {
             console.log('수량 업데이트 성공');
           }
@@ -128,12 +128,13 @@ const handleIpgoRequest = async (req, res) => {
       } else {
         // 검색 결과가 없으면 새로운 제품을 추가
         const insertQuery = ` 
-          INSERT INTO product (product_code, bar_code, name, weight, category, quantity, mfg, exp, location)
-          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+          INSERT INTO product (product_code, bar_code, name, weight, category, quantity, mfg, exp, location, date_management_type, guaranteed_shelf_life)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         `;
-        product_db.query(insertQuery, [searchResult.product_code, searchResult.bar_code, searchResult.name, searchResult.weight, searchResult.category_code, quantity, formattedMfg, formattedExp, 'RCV'], (err, result) => {
+        product_db.query(insertQuery, [searchResult.product_code, searchResult.bar_code, searchResult.name, searchResult.weight, searchResult.category_code, 
+          receivedData.quantity, receivedData.mfg, receivedData.exp, 'RCV', receivedData.date_management_type, receivedData.guaranteed_shelf_life], (err, result) => {
           if (err) {
-            console.error('MySQL 쿼리 오류:', err);
+            console.error('insertQuery 쿼리 오류:', err);
           } else {
             console.log('새로운 제품 추가 성공');
           }
@@ -142,7 +143,7 @@ const handleIpgoRequest = async (req, res) => {
 
 
         insertionResults.push({ success: true });
-      }
+      
 
       // 모든 데이터를 처리한 후 한 번의 응답을 보냄
       res.json({ message: '입고가 진행되었습니다.', results: insertionResults });
